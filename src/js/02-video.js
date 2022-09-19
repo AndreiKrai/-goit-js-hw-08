03bc9f10a20a25189dbee34616393f556d06dd74
AndreiKrai <okraichenko.a@gmail.com>
2022-09-19 16:36:11 +0300

    some changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,10 @@
 import Player from '@vimeo/player';
+console.log(Player);
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const timeKey = 'videoplayer-current-time';
-
+console.log(iframe);
 player.on('timeupdate', durationSaveLocalStorage);
 
 function durationSaveLocalStorage({ seconds }) {
@@ -17,48 +18,5 @@ function newSrart() {
     return;
   }
   const currentVideoTime = localStorage.getItem(timeKey);
-  player
-    .setCurrentTime(currentVideoTime)
-    .then(() => {
-      player.play();
-      // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-
-        default:
-          // some other error occurred
-          break;
-      }
-    });
-}
-import Player from '@vimeo/player';
-player.on('timeupdate', Throttle(durationSaveToStorage, 1000));
-// функция которая ловит ивент лоад, если в локальном хранилище есть запись, достань их
-function newStart() {
-  if (!localStorage.getItem(timeKey)) {
-    return;
-  }
-  const currentVideoTime = localStorage.getItem(timeKey);
-
-  player
-    .setCurrentTime(currentVideoTime)
-    .then(() => {
-      player.play();
-      // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-
-        default:
-          // some other error occurred
-          break;
-      }
-    });
+  player.setCurrentTime(currentVideoTime ?? 0);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,78 @@
+const inputEl = document.querySelector('.feedback-form');
 
+inputEl.addEventListener('input', onFormInput);
+inputEl.addEventListener('submit', onFormSubmit);
+window.addEventListener('load', onPageLoad);
+
+let formData = {};
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onPageLoad(event) {
+  const storageData = localStorage.getItem('feedback-form-state');
+  if (!storageData) return;
+  const storageDataObj = JSON.parse(storageData);
+  console.log(storageDataObj);
+  // використовуємо функцію для переборупо ключам та постановки значення. Використовуємо цикл, бо полів інпуту може бути і 100шт.
+  for (const key in storageDataObj) {
+    inputEl.elements[key].value = storageDataObj[key];
+    //   console.log(storageDataObj[key]);
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+}
+
+// інакше виконання
+// import Throttle from 'lodash.throttle';
+// // ** находим ссылки на необходимые поля ввода данных
+// const form = document.querySelector('.feedback-form');
+// const email = form.querySelector('[name = "email"]');
+// const message = form.querySelector('[name = "message"]');
+// // **создаем переменную для хранения длинного ключа
+// // убираем магические стили и строки
+// const localKey = 'feedback-form-state';
+// // **создаем ссылки на наши импуты и форму
+// form.addEventListener('input', Throttle(storageFormData, 500));
+// form.addEventListener('submit', onFormSubmit);
+// // сохраняет написаное в хранилище
+// window.addEventListener('load', checkStorage);
+// function checkStorage() {
+//   // провераяем хранилище на наличие записаных ключей.
+//   if (!localStorage.getItem(localKey)) return;
+//   const formValue = JSON.parse(localStorage.getItem(localKey));
+//   for (const key in formValue) {
+//     form.elements[key].value = formValue[key];
+//   }
+// email.value = formValue.email;
+// message.value = formValue.message;
+// }
+// **функция отправки
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   // // второй способ достать данные
+//   //   const { email, message } = event.currentTarget.elements;
+//   //   console.dir({ email: email.value, message: message.value });
+//   const saveData = JSON.parse(localStorage.getItem(localKey));
+//   console.dir(saveData);
+//   localStorage.removeItem(localKey);
+//   event.currentTarget.reset();
+// }
+// // //** фукнция хранения данных в хранилище
+// function storageFormData(event) {
+//   const formValue = { email: '', message: '' };
+//   // если в нашем обьекте что то есть то добавь новые к существующим
+//   // что б не затирались данные когда вводим в другом поле
+//   if (localStorage.getItem(localKey)) {
+//     Object.assign(formValue, JSON.parse(localStorage.getItem(localKey)));
+//   }
+//   // создаем локальное хранилище записываем данные приводимые к строке
+//   formValue[event.target.name] = event.target.value.trim();
+//   localStorage.setItem(localKey, JSON.stringify(formValue));
+// }
